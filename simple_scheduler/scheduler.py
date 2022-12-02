93d6d00432ad2cbc482c9185d1c09d373ffea07b
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-12-03 01:04:28 +0900

    Update scheduler.py

--- simple_scheduler/scheduler.py
+++ simple_scheduler/scheduler.py
@@ -21,9 +21,14 @@ class SimpleServer(server.SchedulerServer):
                 minute='40')
             self.scheduler_manager.add_job(
                 job_class_string='simple_scheduler.jobs.curl_job.CurlJob',
-                name='invest_call_long_20',
+                name='invest_call_long_30',
                 pub_args=['http://shclub.synology.me:32773/trade', 'POST',{'gubun' : 'auto' ,'type': 'long','position' : '30','company' : 'next','pc_name' : 'LENOVO-M79'}],
                 minute='40',hour='0')
+             self.scheduler_manager.add_job(
+                job_class_string='simple_scheduler.jobs.curl_job.CurlJob',
+                name='invest_call_short_30',
+                pub_args=['http://shclub.synology.me:32773/trade', 'POST',{'gubun' : 'auto' ,'type': 'short','position' : '30','company' : 'next','pc_name' : 'LENOVO-M79'}],
+                minute='41',hour='0')
             self.scheduler_manager.add_job(
                 job_class_string='simple_scheduler.jobs.curl_job.CurlJob',
                 name='invest_sett',
